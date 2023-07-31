0fcaa7a4475b0cb0aed012e570c992c003fc6354
Franck Chauvel <fchauvel@Francks-MacBook-Air.local>
2023-07-31 16:04:14 +0800

    feature: Show app version number

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    define: {
+        __version__: JSON.stringify(process.env.npm_package_version)
+    }
 });
+g
